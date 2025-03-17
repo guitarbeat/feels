d66060d3f3449c25adf2eaf8658b48b0a93f6319
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2025-03-17 08:54:33 +0000

    chore: remove postcss-preset-env and css-mqpacker from PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    'postcss-preset-env': {
-      features: {
-        'nesting-rules': true
-      }
-    },
-    'css-mqpacker': {} // Combines media queries for better mobile performance
   }
 }
