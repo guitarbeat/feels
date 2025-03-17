fc9d2b1a2b656ff9befa72e05aeb481075568bf4
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2025-03-17 08:48:19 +0000

    feat: add postcss-preset-env for nesting rules and css-mqpacker for improved mobile performance

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,11 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': true
+      }
+    },
+    'css-mqpacker': {} // Combines media queries for better mobile performance
   }
 }
